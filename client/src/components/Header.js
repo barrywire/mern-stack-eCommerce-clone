1c3425dab26c134aafda37427f916afdd0bb531f
BMWire <bmwemawire@gmail.com>
2021-05-30 02:54:01 +0300

    Navigation bar set

---
+++ client/src/components/Header.js
@@ -0,0 +1,41 @@
+import React from "react";
+import { Link } from "react-router-dom";
+const Header = () => {
+  //viers
+  const showNavigation = () => {
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <div className="container-fluid">
+        <Link to="#" className="navbar-brand" href="#">
+          Logo
+        </Link>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarTogglerDemo02"
+          aria-controls="navbarTogglerDemo02"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
+          <ul className="navbar-nav ml-auto mb-2 mb-lg-0">
+            <li className="nav-item">
+              <Link className="nav-link" aria-current="page">
+                Sign Up
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link">Sign In</Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>;
+  };
+  //render
+  return <header id="header">{showNavigation()}</header>;
+};
+
+export default Header;
