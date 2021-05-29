d9da019003e249e2d3999d31cf6413b6f4f90f55
BMWire <bmwemawire@gmail.com>
2021-05-29 21:16:52 +0300

    MongoDB connection successful

---
+++ database/db.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+const connectDB = async () => {
+    try {
+        await mongoose.connect('mongodb+srv://superWebmaster:SuperJan04.@cluster0.mqy7j.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', 
+        {
+            useNewUrlParser:true, 
+        useUnifiedTopology: true
+        }
+    );
+    console.log('Database connection succesful');
+    } catch (error) {
+        console.log(error);
+    } 
+};
+
+module.exports = connectDB
