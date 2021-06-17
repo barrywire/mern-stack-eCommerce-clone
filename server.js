ab767ae8787a450e99cf7e18046672a8e1fd15e9
BMWire <bmwemawire@gmail.com>
2021-06-17 22:22:36 +0300

    Setup signup route for API

--- server.js
+++ server.js
@@ -3,11 +3,13 @@ const app = express();
 const cors = require('cors');
 const morgan = require('morgan');
 const connectDB = require('./database/db');
+const authRoutes = require('./routes/auth');
 
 // Middleware
 app.use(cors());
 app.use(morgan('dev'));
 app.use(express.json());
+app.use('/api/auth', authRoutes)
 
 connectDB();
 app.get('/', (req, res) => {
